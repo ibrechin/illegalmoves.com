2bf17f6af55c43607bc9b34414a07a630ae117a9
Ian Brechin <ian.brechin@gmail.com>
2013-09-01 19:40:31 +0100

    Added RSS

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,14 +6,11 @@ AUTHOR = u'Ian Brechin'
 SITENAME = u'Illegal Moves'
 SITEURL = 'http://localhost:8000'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Europe/London'
 
 DEFAULT_LANG = u'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
+FEED_RSS = 'feeds/rss.xml'
 
 DEFAULT_PAGINATION = 3
 
