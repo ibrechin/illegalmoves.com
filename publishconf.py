ffb04ff8897b705024ec508c649ed22e474a8450
Ian Brechin <zengentleman@gmail.com>
2014-05-08 13:12:20 +0000

    Change domain to www

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://illegalmoves.com/blog'
+SITEURL = 'http://www.illegalmoves.com/blog'
 RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
