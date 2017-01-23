281b2e8b7f561ea02a7f74016c7c19d215d9614f
Ian Brechin <ian.brechin@gmail.com>
2017-01-23 17:31:20 +0000

    Change to HTTPS

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## illegalmoves.com
 
-Repo from which http://www.illegalmoves.com/blog is generated, using Pelican static site generator.
+Repo from which www.illegalmoves.com/blog is generated, using Pelican static site generator.
--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.illegalmoves.com/blog'
+SITEURL = 'https://www.illegalmoves.com/blog'
 RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
